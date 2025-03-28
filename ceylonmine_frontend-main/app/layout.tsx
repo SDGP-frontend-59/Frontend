e82ab81771156fd1bbd479d367e6c0a97a279a17
Senuji De Silva <senujihasara2003@gmail.com>
2025-03-28 22:33:15 +0530

    try

--- ceylonmine_frontend-main/app/layout.tsx
+++ ceylonmine_frontend-main/app/layout.tsx
@@ -11,7 +11,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
+// h
 export const metadata: Metadata = {
   title: "CeylonMine",
   description: "Your mining solutions platform",
